07989ccb67846ed14baf0ac325d1eccb5f9e97c0
dlukinichev <dan683012@gmail.com>
2024-06-27 21:19:29 +0300

    test

--- file_iterator/FileSystemIterator.py
+++ file_iterator/FileSystemIterator.py
@@ -1,11 +1,35 @@
+import os
+
+
 class FileSystemIterator:
-    def __init__(self, root, only_files, only_dirs, pattern):
-        """
-        Инициализация объекта
-        :param root: корневой каталог
-        :param only_files: итерgnbftgrthrthироваться только по файлам
-        :param only_dirs: итерироваться только по директerewrerververориям
-        :param pattern: итерироваться только по объектам файловой системы, содержащим в имени строку pattern
-        """
- 
-       # Реализация
+    def __init__(self, root_path, only_files=False, only_dirts=False, pattern=None):
+        self.root_path = root_path
+        self.only_files = only_files
+        self.only_dirts = only_dirts
+        self.pattern = pattern
+        self._iterator = self._create_iterator()
+
+    def _create_iterator(self):
+        for dirpath, dirnames, filenames in os.walk(self.root_path):
+            if not self.only_dirts:
+                for filename in filenames:
+                    if not self.pattern or self.pattern in filename:
+                        yield os.path.join(dirpath, filename)
+            if not self.only_files:
+                for dirname in dirnames:
+                    if not self.pattern or self.pattern in dirname:
+                        yield os.path.join(dirpath, dirname)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return next(self._iterator)
+
+
+for item in FileSystemIterator('/', False, False, None):
+    print(item)
+
+print('#########################################')
+
+print(next(FileSystemIterator('/', False, False, None)))
