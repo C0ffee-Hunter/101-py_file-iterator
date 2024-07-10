79b479d7b9ad9d74487a2b364ceecfb7317676fa
dlukinichev <dan683012@gmail.com>
2024-07-10 10:48:51 +0300

    Исправление ошибок

--- file_iterator/FileSystemIterator.py
+++ file_iterator/FileSystemIterator.py
@@ -3,6 +3,8 @@ import os
 
 class FileSystemIterator:
     def __init__(self, root_path, only_files=False, only_dirts=False, pattern=None):
+        if not os.path.exists(root_path):
+            raise FileNotFoundError(f"Root directory '{root_path}' does not exist")
         self.root_path = root_path
         self.only_files = only_files
         self.only_dirts = only_dirts
@@ -10,6 +12,8 @@ class FileSystemIterator:
         self._iterator = self._create_iterator()
 
     def _create_iterator(self):
+        if self.only_files and self.only_dirts:
+            raise ValueError("Cannot set both 'only_files' and 'only_dirts' to True")
         for dirpath, dirnames, filenames in os.walk(self.root_path):
             if not self.only_dirts:
                 for filename in filenames:
