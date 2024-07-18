4e25c9689d1da602a2e43cc32c67a2e8d513ef93
dlukinichev <dan683012@gmail.com>
2024-07-18 14:07:21 +0300

    Исправление ошибок

--- file_iterator/FileSystemIterator.py
+++ file_iterator/FileSystemIterator.py
@@ -15,14 +15,15 @@ class FileSystemIterator:
         if self.only_files and self.only_dirts:
             raise ValueError("Cannot set both 'only_files' and 'only_dirts' to True")
         for dirpath, dirnames, filenames in os.walk(self.root_path):
-            if not self.only_dirts:
-                for filename in filenames:
-                    if not self.pattern or self.pattern in filename:
-                        yield os.path.join(dirpath, filename)
-            if not self.only_files:
-                for dirname in dirnames:
-                    if not self.pattern or self.pattern in dirname:
-                        yield os.path.join(dirpath, dirname)
+            yield from self._process_items(dirpath, dirnames, filenames)
+
+    def _process_items(self, dirpath, dirnames, filenames):
+        if not self.only_dirts:
+            yield from (os.path.join(dirpath, filename) for filename in filenames if
+                        not self.pattern or self.pattern in filename)
+        if not self.only_files:
+            yield from (os.path.join(dirpath, dirname) for dirname in dirnames if
+                        not self.pattern or self.pattern in dirname)
 
     def __iter__(self):
         return self
@@ -31,9 +32,3 @@ class FileSystemIterator:
         return next(self._iterator)
 
 
-for item in FileSystemIterator('/', False, False, None):
-    print(item)
-
-print('#########################################')
-
-print(next(FileSystemIterator('/', False, False, None)))
